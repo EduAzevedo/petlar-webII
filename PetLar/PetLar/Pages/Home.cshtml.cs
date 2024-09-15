6713a97e428af9d32338bc230d8109f3b9b8f746
Eduardo Azevedo <89713966+EduAzevedo@users.noreply.github.com>
2024-09-15 17:31:01 -0300

    Ajustes na LandingPage e criacao da Home

---
+++ PetLar/PetLar/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetLar.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Lógica para carregar dados necessários na homepage
+        }
+    }
+}
--- PetLar/PetLar/Program.cs
+++ PetLar/PetLar/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PetLar.Data;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>

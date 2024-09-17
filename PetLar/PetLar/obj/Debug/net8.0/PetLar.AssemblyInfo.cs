dd2f2eddee33f213a3a002ba291fbf658f025dcf
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-09-17 20:04:05 -0300

    trash

--- PetLar/PetLar/obj/Debug/net8.0/PetLar.AssemblyInfo.cs
+++ PetLar/PetLar/obj/Debug/net8.0/PetLar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetLar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee73e4561d23a80707bcf0c04eb422d9a524e1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edbb3a9c49303d878a95a77783848b001c85efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetLar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetLar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
